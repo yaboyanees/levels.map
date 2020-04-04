56a2b016f9b5cae580890e89c34193eb42f40f00
Anees <yaboyanees@users.noreply.github.com>
2020-04-03 23:24:24 -0400

    Update leaflet-gsheets.js

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -7,15 +7,9 @@
 
 // init() is called as soon as the page loads
 function init() {
-  // PASTE YOUR URLs HERE
-  // these URLs come from Google Sheets 'shareable link' form
-  // the first is the polygon layer and the second the points
-  var polyURL =
-    "https://docs.google.com/spreadsheets/d/1lGATGrPOwjmh7VoiwstEEUDalAQ3EwtwX6uSjm8PjQk/edit?usp=sharing";
   var pointsURL =
-    "https://docs.google.com/spreadsheets/d/16YWuec5c6Gx9QCcDex_O7QiqdwIa3qxVhjSMnl8tmn4/edit?usp=sharing";
+    "https://docs.google.com/spreadsheets/d/1MvtG-2QBoLvjr3QSmvQKNsZ5eM82JoGJtzn0VAL_54k/edit?usp=sharing";
 
-  Tabletop.init({ key: polyURL, callback: addPolygons, simpleSheet: true });
   Tabletop.init({ key: pointsURL, callback: addPoints, simpleSheet: true }); // simpleSheet assumes there is only one table and automatically sends its data
 }
 window.addEventListener("DOMContentLoaded", init);
@@ -56,82 +50,9 @@ map.on("click", function() {
   sidebar.close(panelID);
 });
 
-// These are declared outisde the functions so that the functions can check if they already exist
-var polygonLayer;
 var pointGroupLayer;
 
-// The form of data must be a JSON representation of a table as returned by Tabletop.js
-// addPolygons first checks if the map layer has already been assigned, and if so, deletes it and makes a fresh one
-// The assumption is that the locally stored JSONs will load before Tabletop.js can pull the external data from Google Sheets
-function addPolygons(data) {
-  if (polygonLayer != null) {
-    // If the layer exists, remove it and continue to make a new one with data
-    polygonLayer.remove();
-  }
-
-  // Need to convert the Tabletop.js JSON into a GeoJSON
-  // Start with an empty GeoJSON of type FeatureCollection
-  // All the rows will be inserted into a single GeoJSON
-  var geojsonStates = {
-    type: "FeatureCollection",
-    features: []
-  };
-
-  for (var row in data) {
-    // The Sheets data has a column 'include' that specifies if that row should be mapped
-    if (data[row].include == "y") {
-      var coords = JSON.parse(data[row].geometry);
-
-      geojsonStates.features.push({
-        type: "Feature",
-        geometry: {
-          type: "MultiPolygon",
-          coordinates: coords
-        },
-        properties: {
-          name: data[row].name,
-          summary: data[row].summary,
-          state: data[row].state,
-          local: data[row].local
-        }
-      });
-    }
-  }
-
-  // The polygons are styled slightly differently on mouse hovers
-  var polygonStyle = { color: "#2ca25f", fillColor: "#99d8c9", weight: 1.5 };
-  var polygonHoverStyle = { color: "green", fillColor: "#2ca25f", weight: 3 };
-
-  polygonLayer = L.geoJSON(geojsonStates, {
-    onEachFeature: function(feature, layer) {
-      layer.on({
-        mouseout: function(e) {
-          e.target.setStyle(polygonStyle);
-        },
-        mouseover: function(e) {
-          e.target.setStyle(polygonHoverStyle);
-        },
-        click: function(e) {
-          // This zooms the map to the clicked polygon
-          // map.fitBounds(e.target.getBounds());
-
-          // if this isn't added, then map.click is also fired!
-          L.DomEvent.stopPropagation(e);
-
-          document.getElementById("sidebar-title").innerHTML =
-            e.target.feature.properties.name;
-          document.getElementById("sidebar-content").innerHTML =
-            e.target.feature.properties.summary;
-          sidebar.open(panelID);
-        }
-      });
-    },
-    style: polygonStyle
-  }).addTo(map);
-}
-
-// addPoints is a bit simpler, as no GeoJSON is needed for the points
-// It does the same check to overwrite the existing points layer once the Google Sheets data comes along
+ 
 function addPoints(data) {
   if (pointGroupLayer != null) {
     pointGroupLayer.remove();
@@ -139,7 +60,7 @@ function addPoints(data) {
   pointGroupLayer = L.layerGroup().addTo(map);
 
   for (var row = 0; row < data.length; row++) {
-    var marker = L.marker([data[row].lat, data[row].long]).addTo(
+    var marker = L.marker([data[row].Latitude, data[row].Longitude]).addTo(
       pointGroupLayer
     );
 
@@ -149,8 +70,8 @@ function addPoints(data) {
     // COMMENT THE NEXT 14 LINES TO DISABLE SIDEBAR FOR THE MARKERS
     marker.feature = {
       properties: {
-        location: data[row].location,
-        category: data[row].category
+        Company: data[row].Company,
+        Roles: data[row].Roles
       }
     };
     marker.on({
@@ -168,7 +89,7 @@ function addPoints(data) {
     var icon = L.AwesomeMarkers.icon({
       icon: "info-sign",
       iconColor: "white",
-      markerColor: getColor(data[row].category),
+      markerColor: getColor(data[row].Company),
       prefix: "glyphicon",
       extraClasses: "fa-rotate-0"
     });
@@ -178,7 +99,7 @@ function addPoints(data) {
 
 // Returns different colors depending on the string passed
 // Used for the points layer
-function getColor(type) {
+/*function getColor(type) {
   switch (type) {
     case "Coffee Shop":
       return "green";
@@ -187,4 +108,4 @@ function getColor(type) {
     default:
       return "green";
   }
-}
+}*/
