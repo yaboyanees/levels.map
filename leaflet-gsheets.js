e977885426aa2c363e6ef970d6d2af3e147d9024
Anees <yaboyanees@users.noreply.github.com>
2020-04-04 10:24:23 -0400

    Adjusted configs for map and markers

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -1,7 +1,5 @@
-/* global L Tabletop */
-
 /*
- * Script to display two tables from Google Sheets as point and polygon layers using Leaflet
+ * Script to display two tables from Google Sheets as point using Leaflet
  * The Sheets are then imported using Tabletop.js and overwrite the initially laded layers
  */
 
@@ -15,44 +13,18 @@ function init() {
 window.addEventListener("DOMContentLoaded", init);
 
 // Create a new Leaflet map centered on the continental US
-var map = L.map("map").setView([40, -100], 4);
+var map = L.map("map").setView([28, -0], 2.5);
 
-// This is the Carto Positron basemap
-var basemap = L.tileLayer(
-  "https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}{r}.png",
-  {
-    attribution:
-      "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://cartodb.com/attributions'>CartoDB</a>",
-    subdomains: "abcd",
-    maxZoom: 19
-  }
-);
-basemap.addTo(map);
-
-var sidebar = L.control
-  .sidebar({
-    container: "sidebar",
-    closeButton: true,
-    position: "right"
-  })
-  .addTo(map);
-
-let panelID = "my-info-panel";
-var panelContent = {
-  id: panelID,
-  tab: "<i class='fa fa-bars active'></i>",
-  pane: "<p id='sidebar-content'></p>",
-  title: "<h2 id='sidebar-title'>No state selected</h2>"
-};
-sidebar.addPanel(panelContent);
-
-map.on("click", function() {
-  sidebar.close(panelID);
-});
+// basemap
+var CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 19
+}).addTo(map);
 
 var pointGroupLayer;
 
- 
+
 function addPoints(data) {
   if (pointGroupLayer != null) {
     pointGroupLayer.remove();
@@ -64,48 +36,23 @@ function addPoints(data) {
       pointGroupLayer
     );
 
-    // UNCOMMENT THIS LINE TO USE POPUPS
-    //marker.bindPopup('<h2>' + data[row].location + '</h2>There's a ' + data[row].level + ' ' + data[row].category + ' here');
+    // POPUPS
+    marker.bindPopup('<h4 style="margin:0 0 3px 0;"><a href="' + data[row].ApplyLink + '">' + data[row].Company + '</a></h4>(' + data[row].Locations + ')<br/><br/>Hiring:' + data[row].Roles);
 
-    // COMMENT THE NEXT 14 LINES TO DISABLE SIDEBAR FOR THE MARKERS
-    marker.feature = {
-      properties: {
-        Company: data[row].Company,
-        Roles: data[row].Roles
-      }
-    };
-    marker.on({
-      click: function(e) {
-        L.DomEvent.stopPropagation(e);
-        document.getElementById("sidebar-title").innerHTML =
-          e.target.feature.properties.location;
-        document.getElementById("sidebar-content").innerHTML =
-          e.target.feature.properties.category;
-        sidebar.open(panelID);
-      }
+    var companyIcon = L.Icon.extend({
+        options: {
+            iconSize: [28, 24],
+            //shadowSize:   [50, 64],
+            //iconAnchor:   [22, 94],
+            //shadowAnchor: [4, 62],
+            //popupAnchor:  [-3, -76]
+        }
     });
 
-    // AwesomeMarkers is used to create fancier icons
-    var icon = L.AwesomeMarkers.icon({
-      icon: "info-sign",
-      iconColor: "white",
-      markerColor: getColor(data[row].Company),
-      prefix: "glyphicon",
-      extraClasses: "fa-rotate-0"
-    });
-    marker.setIcon(icon);
-  }
-}
+    var logoIcon = new companyIcon(
+        { iconUrl: data[row].Logo});
 
-// Returns different colors depending on the string passed
-// Used for the points layer
-function getColor(type) {
-  switch (type) {
-    case "Airtable":
-      return "green";
-    case "Restaurant":
-      return "blue";
-    default:
-      return "red";
+    marker.setIcon(logoIcon);
   }
+  //console.log(data)
 }
